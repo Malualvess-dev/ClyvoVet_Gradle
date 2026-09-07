d336ffba6c1103877dc31a15ff1958a91292be73
Malu <maluaquino1007@gmail.com>
2026-09-07 18:07:14 -0300

    Alterando agenda com JWT

--- src/main/java/br/com/fiap/clyvovet/controller/AgendaController.java
+++ src/main/java/br/com/fiap/clyvovet/controller/AgendaController.java
@@ -1,6 +1,5 @@
 package br.com.fiap.clyvovet.controller;
 
-
 import br.com.fiap.clyvovet.dto.request.AgendaRequest;
 import br.com.fiap.clyvovet.dto.response.AgendaResponse;
 import br.com.fiap.clyvovet.dto.response.ApiResponse;
@@ -8,10 +7,12 @@ import br.com.fiap.clyvovet.service.AgendaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -23,10 +24,13 @@ public class AgendaController {
 
     private final AgendaService agendaService;
 
-    public AgendaController(AgendaService agendaService) {
+    public AgendaController(
+            AgendaService agendaService
+    ) {
         this.agendaService = agendaService;
     }
 
+    // CREATE
     @Operation(summary = "Cria uma nova agenda")
     @PostMapping
     public ResponseEntity<ApiResponse> createAgenda(
@@ -41,34 +45,76 @@ public class AgendaController {
         );
     }
 
+    // READ POR ID
     @Operation(summary = "Busca agenda por id")
     @GetMapping("/{id}")
-    public ResponseEntity<AgendaResponse> readAgenda(@PathVariable Integer id) {
-        return new ResponseEntity<>(agendaService.readAgenda(id), HttpStatus.OK);
+    public ResponseEntity<AgendaResponse> readAgenda(
+            @PathVariable Integer id
+    ) {
+
+        return new ResponseEntity<>(
+                agendaService.readAgenda(id),
+                HttpStatus.OK
+        );
     }
 
+    // READ TODOS
     @Operation(summary = "Lista agendas")
     @GetMapping
-    public ResponseEntity<Page<AgendaResponse>> readAgendas(Pageable pageable) {
-        return new ResponseEntity<>(agendaService.read(pageable), HttpStatus.OK);
+    public ResponseEntity<Page<AgendaResponse>> readAgendas(
+            @ParameterObject Pageable pageable
+    ) {
+
+        return new ResponseEntity<>(
+                agendaService.read(pageable),
+                HttpStatus.OK
+        );
     }
 
+    // READ POR PERÍODO
     @Operation(summary = "Busca agenda por período")
     @GetMapping("/periodo")
     public ResponseEntity<Page<AgendaResponse>> readByPeriodo(
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime inicio,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime fim,
-            Pageable pageable
+
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime inicio,
+
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime fim,
+
+            @ParameterObject Pageable pageable
     ) {
-        return new ResponseEntity<>(agendaService.readByPeriodo(inicio, fim, pageable), HttpStatus.OK);
+
+        return new ResponseEntity<>(
+                agendaService.readByPeriodo(
+                        inicio,
+                        fim,
+                        pageable
+                ),
+                HttpStatus.OK
+        );
     }
 
+    // UPDATE
     @Operation(summary = "Atualiza agenda")
     @PutMapping("/{id}")
-    public ResponseEntity<AgendaResponse> updateAgenda(@PathVariable Integer id, @Valid @RequestBody AgendaRequest agendaRequest) {
-        return new ResponseEntity<>(agendaService.update(id, agendaRequest), HttpStatus.OK);
+    public ResponseEntity<AgendaResponse> updateAgenda(
+            @PathVariable Integer id,
+            @Valid @RequestBody AgendaRequest agendaRequest
+    ) {
+
+        return new ResponseEntity<>(
+                agendaService.update(
+                        id,
+                        agendaRequest
+                ),
+                HttpStatus.OK
+        );
     }
 
+    // DELETE
     @Operation(summary = "Remove agenda")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteAgenda(
--- src/main/java/br/com/fiap/clyvovet/service/AgendaService.java
+++ src/main/java/br/com/fiap/clyvovet/service/AgendaService.java
@@ -9,10 +9,11 @@ import br.com.fiap.clyvovet.model.Usuario;
 import br.com.fiap.clyvovet.repository.AgendaRepository;
 import br.com.fiap.clyvovet.repository.UsuarioRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
 
+import java.time.LocalDateTime;
 
 @Service
 public class AgendaService {
@@ -21,41 +22,65 @@ public class AgendaService {
     private final UsuarioRepository usuarioRepository;
     private final AgendaMapper agendaMapper;
 
-    public AgendaService(AgendaRepository agendaRepository, UsuarioRepository usuarioRepository, AgendaMapper agendaMapper) {
+    public AgendaService(
+            AgendaRepository agendaRepository,
+            UsuarioRepository usuarioRepository,
+            AgendaMapper agendaMapper
+    ) {
         this.agendaRepository = agendaRepository;
         this.usuarioRepository = usuarioRepository;
         this.agendaMapper = agendaMapper;
     }
 
-    //CRUD
-
-    //CREATE
+    // CREATE
     public AgendaResponse create(AgendaRequest request) {
-        Usuario usuario = usuarioRepository.findById(request.usuarioId())
-                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+
+        Usuario usuario = usuarioRepository
+                .findById(request.usuarioId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Usuário não encontrado"
+                        )
+                );
 
         Agenda agenda = new Agenda();
+
         agenda.setTitulo(request.titulo());
         agenda.setDataHora(request.dataHora());
         agenda.setAnotacao(request.anotacao());
         agenda.setUsuario(usuario);
 
-        return agendaMapper.agendaToResponse(agendaRepository.save(agenda));
+        return agendaMapper.agendaToResponse(
+                agendaRepository.save(agenda)
+        );
     }
 
-    //READ
-
+    // READ POR ID
     public AgendaResponse readAgenda(Integer id) {
-        Agenda agenda = agendaRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Agenda não encontrada"));
+
+        Agenda agenda = agendaRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Agenda não encontrada"
+                        )
+                );
 
         return agendaMapper.agendaToResponse(agenda);
     }
 
+    // READ TODOS
     public Page<AgendaResponse> read(Pageable pageable) {
 
+        Pageable pageableSemOrdenacao =
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize()
+                );
+
         Page<AgendaResponse> agendas =
-                agendaRepository.findAll(pageable)
+                agendaRepository
+                        .findAll(pageableSemOrdenacao)
                         .map(agendaMapper::agendaToResponse);
 
         if (agendas.isEmpty()) {
@@ -67,33 +92,79 @@ public class AgendaService {
         return agendas;
     }
 
-    public Page<AgendaResponse> readByPeriodo(LocalDateTime inicio, LocalDateTime fim, Pageable pageable) {
-        return agendaRepository.findByDataHoraBetween(inicio, fim, pageable)
-                .map(agendaMapper::agendaToResponse);
+    // READ POR PERÍODO
+    public Page<AgendaResponse> readByPeriodo(
+            LocalDateTime inicio,
+            LocalDateTime fim,
+            Pageable pageable
+    ) {
+
+        Pageable pageableSemOrdenacao =
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize()
+                );
+
+        Page<AgendaResponse> agendas =
+                agendaRepository
+                        .findByDataHoraBetween(
+                                inicio,
+                                fim,
+                                pageableSemOrdenacao
+                        )
+                        .map(agendaMapper::agendaToResponse);
+
+        if (agendas.isEmpty()) {
+            throw new ResourceNotFoundException(
+                    "Nenhuma agenda encontrada no período informado"
+            );
+        }
+
+        return agendas;
     }
 
-    //UPDATE
+    // UPDATE
+    public AgendaResponse update(
+            Integer id,
+            AgendaRequest request
+    ) {
 
-    public AgendaResponse update(Integer id, AgendaRequest request) {
-        Agenda agenda = agendaRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Agenda não encontrada"));
+        Agenda agenda = agendaRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Agenda não encontrada"
+                        )
+                );
 
-        Usuario usuario = usuarioRepository.findById(request.usuarioId())
-                .orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+        Usuario usuario = usuarioRepository
+                .findById(request.usuarioId())
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Usuário não encontrado"
+                        )
+                );
 
         agenda.setTitulo(request.titulo());
         agenda.setDataHora(request.dataHora());
         agenda.setAnotacao(request.anotacao());
         agenda.setUsuario(usuario);
 
-        return agendaMapper.agendaToResponse(agendaRepository.save(agenda));
+        return agendaMapper.agendaToResponse(
+                agendaRepository.save(agenda)
+        );
     }
 
-    //DELETE
-
+    // DELETE
     public void delete(Integer id) {
-        Agenda agenda = agendaRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Agenda não encontrada"));
+
+        Agenda agenda = agendaRepository
+                .findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Agenda não encontrada"
+                        )
+                );
 
         agendaRepository.delete(agenda);
     }
