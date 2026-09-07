69c9481d455d0991edf22afe7d4c25cfcaf893c1
Malu <maluaquino1007@gmail.com>
2026-09-07 17:45:30 -0300

    Veterinario finalizado e configurado com JWT

--- src/main/java/br/com/fiap/clyvovet/model/Veterinario.java
+++ src/main/java/br/com/fiap/clyvovet/model/Veterinario.java
@@ -2,6 +2,8 @@ package br.com.fiap.clyvovet.model;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "TB_VETERINARIO")
 public class Veterinario {
@@ -19,20 +21,53 @@ public class Veterinario {
     @Column(name = "ID_VETERINARIO")
     private Integer veterinarioId;
 
-    @Column(name = "NOME_VETERINARIO", length = 100, nullable = false)
+    // Coluna NOME
+    @Column(
+            name = "NOME",
+            length = 100,
+            nullable = false
+    )
     private String nomeVeterinario;
 
-    @Column(name = "CRMV", length = 20, nullable = false, unique = true)
+    // Coluna NOME_VETERINARIO
+    @Column(
+            name = "NOME_VETERINARIO",
+            length = 100,
+            nullable = false
+    )
+    private String nomeVeterinarioBanco;
+
+    @Column(
+            name = "CRMV",
+            length = 20,
+            nullable = false,
+            unique = true
+    )
     private String crmv;
 
-    @Column(name = "TELEFONE", length = 20)
+    @Column(
+            name = "TELEFONE",
+            length = 20
+    )
     private String telefone;
 
-    @Column(name = "ESPECIALIDADE", length = 100)
+    @Column(
+            name = "ESPECIALIDADE",
+            length = 100
+    )
     private String especialidade;
 
+    @Column(
+            name = "DT_NASCIMENTO",
+            nullable = false
+    )
+    private LocalDate dataNascimento;
+
     @ManyToOne
-    @JoinColumn(name = "ID_USUARIO")
+    @JoinColumn(
+            name = "ID_USUARIO",
+            nullable = false
+    )
     private Usuario usuario;
 
     // CONSTRUTOR
@@ -40,7 +75,7 @@ public class Veterinario {
     public Veterinario() {
     }
 
-    // GET E SET
+    // GETTERS E SETTERS
 
     public Integer getVeterinarioId() {
         return veterinarioId;
@@ -56,6 +91,14 @@ public class Veterinario {
 
     public void setNomeVeterinario(String nomeVeterinario) {
         this.nomeVeterinario = nomeVeterinario;
+    }
+
+    public String getNomeVeterinarioBanco() {
+        return nomeVeterinarioBanco;
+    }
+
+    public void setNomeVeterinarioBanco(String nomeVeterinarioBanco) {
+        this.nomeVeterinarioBanco = nomeVeterinarioBanco;
     }
 
     public String getCrmv() {
@@ -80,6 +123,14 @@ public class Veterinario {
 
     public void setEspecialidade(String especialidade) {
         this.especialidade = especialidade;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
     }
 
     public Usuario getUsuario() {
